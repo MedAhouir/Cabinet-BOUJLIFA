e47824bd9ece629d5a1d19f29aa2b8c4cdf345b6
MedAhouir <mohamedahouir@gmail.com>
2025-02-07 15:06:29 +0100

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://cabinet-boujlifa.vercel.app/', // Replace with your actual domain
+    generateRobotsTxt: true, // Creates a robots.txt file
+  };
+  
